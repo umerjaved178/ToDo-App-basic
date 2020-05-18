7444ee2b0f91f57d98dc720e7b8901822c0e3876
umerjaved178 <umerjaved178@gmail.com>
2020-05-19 02:00:41 +0500

    whitenoise file

--- todo/settings.py
+++ todo/settings.py
@@ -49,6 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #simplified static file serving, white noise
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'todo.urls'
